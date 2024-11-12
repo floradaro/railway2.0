a8934d81f1df4ddbb385257386973807269c73ca
Florencia Adaro <flor.adaro@gmail.com>
2024-11-12 02:54:24 -0300

    prueba4

--- app.py
+++ app.py
@@ -85,4 +85,4 @@ def delete_user(id):
     return jsonify({'message': 'Usuario eliminado exitosamente'})
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get("PORT", 3306)))
