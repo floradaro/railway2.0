2d43710cb7a1c74b2865688b868433801db58357
Florencia Adaro <flor.adaro@gmail.com>
2024-11-12 02:26:16 -0300

    prueba1

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+import os
 import mysql.connector
 
 app = Flask(__name__)
@@ -30,4 +31,4 @@ def test_db():
         conn.close()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
