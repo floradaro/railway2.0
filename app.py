94a82ad554ed9fbdb020a01728b7baab676463d6
flor.adaro <flor.adaro@gmail.com>
2025-05-16 23:00:58 -0300

    Ajuste de rutas, se elimina section de index

--- app.py
+++ app.py
@@ -185,7 +185,7 @@ def login():
 
 @app.before_request
 def check_login():
-    public_routes = ['home', 'login','contact','signup_page', 'create_user', 'reset', 'static', 'check_login_status', 'sizeTable']
+    public_routes = ['home', 'login','contact','signup_page', 'create_user', 'reset', 'static', 'check_login_status', 'sizeTable', 'detail', 'base']
     
     if request.endpoint and request.endpoint not in public_routes and not session.get('logged_in'):
         return redirect(url_for('login'))
